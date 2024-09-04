655f8409139dc5db742dd4dd29b6d21ff7cb913c
Algin Anto <alginanto143@gmail.com>
2024-09-04 14:31:24 +0530

    json format result

--- infrastructure/gemini/geminiService.js
+++ infrastructure/gemini/geminiService.js
@@ -1,13 +1,14 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const QuotesRepository = require("../../application/interfaces/QuotesRepository");
-const genAi = new GoogleGenerativeAI("API_KEY");
-const model = genAi.getGenerativeModel({ model: "Gemini-1.5-pro" });
+const genAI = new GoogleGenerativeAI(process.env.GEMINIAI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 class GeminiService extends QuotesRepository {
   async getDailyQuotes() {
-    const promt = `ADD YOUR PROMT`;
+    const promt = `Tell me a 5 jock in json format`;
     const result = await model.generateContent(promt);
     const response = await result.response;
+    console.log(response.text());
     const text = response.text();
     return text;
   }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const meditationRoutes = require("./adapters/routes/meditationRoutes");
 const songRoutes = require("./adapters/routes/songRoutes");
 
